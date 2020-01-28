058a2b3ccbbca826ac0414a7e08e2c762ee93993
mattemario1 <sirmathioso@gmail.com>
2020-01-28 15:28:32 +0100

    Servo controll continue

--- Software/Motor_driver/Microcontroller-2/Microcontroller-2/main.asm
+++ Software/Motor_driver/Microcontroller-2/Microcontroller-2/main.asm
@@ -1,14 +1,36 @@
 ;motor processor
 
 
+	.dseg
+	.org	SRAM_START
+POSU:
+	.byte	1	;Upper slab possission
+POSL:
+	.byte	1	;Lower slab possission
+
+
+	.cseg
+SERVO_STOP:	
+	.db		0x00, 0x00	;***fixa sen***
+SERVO_RR:	;refresh rate
+	.db		0x00, 0x00	;***fixa sen***
+SERVO_CW:	;clockwise
+	.db		0x00, 0x00	;***fixa sen***
+SERVO_CCW:	;counter clockwise
+	.db		0x00, 0x00	;***fixa sen***
+
+
+
 	;---code
 	.cseg
 	.org	0
 	jmp		START
 	.org	INT0addr
-	jmp		DRUM_TRIGGER
+	jmp		UPPER_TRIGGER
+	.org	INT1addr
+	jmp		LOWER_TRIGGER
 	.org	0x10
-	jmp		TIMER_1
+	jmp		SERVO_DIRECTION
 
 
 
@@ -19,12 +41,13 @@ START:
 	out		SPL, r16
 	;
     call	HW_INIT
+	
     rjmp	start
 ;---------------------------------------
 
 
 ;***Eatch time the the drum passes a color dump***
-DRUM_TRIGGER:
+UPPER_TRIGGER:
 	;push all used
 	push	r16
 	in		r16, SREG
@@ -38,7 +61,11 @@ DRUM_TRIGGER:
 	reti
 ;----------------------------------------
 
-TIMER_1:
+
+
+
+;***Eatch time the the drum passes a color dump***
+LOWER_TRIGGER:
 	;push all used
 	push	r16
 	in		r16, SREG
@@ -50,19 +77,48 @@ TIMER_1:
 	out		r16, SREG
 	pop		r16
 	reti
+;----------------------------------------
+
+
+SERVO_DIRECTION:
+	;push all used
+	push	r16
+	in		r16, SREG
+	push	r16
+	;
+
+	;pull all used
+	pop		r16
+	out		r16, SREG
+	pop		r16
+	reti
+;----------------------------------------
+
+
+
+
+
+CHANGE_SERVO:
+	lpm		r16, Z+
+	out		ICR1H, r16
+	lpm		r16, Z
+	out		ICR1L, r16
+	ret
+;----------------------------------------
+
+
 
 
 HW_INIT:
-
-
 	;interupt for drum trigger
-	ldi		r16, (1 << ISC00)|(1 << ISC01)
+	ldi		r16, (1<<ISC00)|(1<<ISC01)|(1<<ISC11)|(1<<ISC10)
 	out		MCUCR, r16
-	ldi		r16, (1 << INT0)
+	ldi		r16, (1<<INT0)|(1<<INT1)
 	out		GICR, r16
 
+
 TIMER_INIT:
-	; Setting counter mode
+	; Setting counter mode 
 	ldi		r16, (1<<WGM13)|(1<<WGM12)
 	out		TCCR1B, r16
 
