db4b96f9212bf5323f836a872e4b872ffc1ffc6e
gaton12 <gaton12@icloud.com>
2020-02-04 12:19:48 +0100

    Update main.asm
    Test av TWI mellan ATMEGA och RGB

--- Software/TWI/masterCodeTest/main.asm
+++ Software/TWI/masterCodeTest/main.asm
@@ -8,8 +8,23 @@
 
 ; Replace with your application code
 
-/// i2c/TWI RGB INITIALIZER ///
-		
+/// i2c/TWI MT code ///
+.equ IDcommand = 0x12
+
+init:
+	ldi r16, HIGH(RAMEND)
+	out SPH, r16
+	ldi r16, LOW(RAMEND)
+	out SPL, r16
+
+	ldi r16, 0xFF
+	out DDRA, r16
+
+main:
+	call start
+	jmp sendAddress
+
+start:
 	ldi r16, (1<<TWINT) | (1<<TWSTA) | (1<<TWEN)
 	out TWCR, r16 //start
 
@@ -22,8 +37,14 @@ wait1:
 	andi r16, 0xF8
 	cpi r16, 0x08
 	brne ERROR //koll om status register är rätt annars error
+	ret
+/////////////////////
 
-	ldi r16 SLA+W //SLA is slave adress, W is write bit
+
+sendAddress:
+	ldi r16, 0x29 //SLA is slave adress, W is write bit
+	lsl r16
+	ori r16, 0 //sätter write bit
 	out TWDR, r16
 	ldi r16, (1<<TWINT) | (1<<TWEN)
 	out TWCR, r16 //skicka SLA+W för att aktivera MT mode
@@ -37,9 +58,11 @@ wait2:
 	cpi r16, 0x18
 	brne ERROR //koll om status register är rätt annars error
 
-	// DATA KAN NU LÄGGAS TILL TWDR //
+///////////////////////
 
-	ldi r16, DATA
+	// DATA KAN NU LÄGGAS TILL TWDR //
+sendData:
+	ldi r16, IDcommand //Här laddas data
 	out TWDR, r16 //laddad
 
 	ldi r16, (1<<TWINT) | (1<<TWEN)
@@ -53,3 +76,43 @@ wait3:
 	andi r16, 0xF8
 	cpi r16, 0x28
 	brne ERROR //koll om status register är rätt annars error
+/////////////////////////
+
+
+readData:
+	call start
+
+sendAddressRead:
+	ldi r16, 0x29 //SLA is slave adress, W is write bit
+	lsl r16
+	ori r16, 1 //sätter write bit
+	out TWDR, r16
+	ldi r16, (1<<TWINT) | (1<<TWEN)
+	out TWCR, r16 //skicka SLA+W för att aktivera MT mode
+wait2Read:
+	in r16, TWCR
+	sbrs r16, TWINT
+	rjmp wait2 //vänta på ok till SLA+W
+
+	in r16, TWSR
+	andi r16, 0xF8
+	cpi r16, 0x18
+	brne ERROR //koll om status register är rätt annars error
+
+wait3Read:
+	in r16, TWCR
+	sbrs r16, TWINT
+	rjmp wait3Read
+
+	in r16, TWDR
+	out PORTA, r16
+
+doneLoop:
+	rjmp doneLoop
+
+
+
+ERROR:
+	ldi r16, 0b01010101
+	out porta, r16
+	rjmp error
