c5a14d5f26af929a75d7f00ee35bfef28e0cc5d7
txcpnae <57444167+txcpnae@users.noreply.github.com>
2020-03-12 09:43:18 +0100

    16x16 multiplikation
    Får ev ändra vilka register den använder. Får avvakta tills den nya divisionen funkar???

--- Software/TWI/masterCodeTest/main.asm
+++ Software/TWI/masterCodeTest/main.asm
@@ -1,181 +1,56 @@
-/// i2c/TWI MT code ///
-.equ IDcommand = 0x12
-.equ redColor = 0x17
+ldi r16, HIGH(RAMEND)
+out SPH, r16
+ldi r16, LOW(RAMEND)
+out SPL, r16
 
-init:
-	ldi r16, HIGH(RAMEND)
-	out SPH, r16
-	ldi r16, LOW(RAMEND)
-	out SPL, r16
-
-	ldi r16, 0xFF
-	out DDRA, r16
-	ldi r16, 0x00
-	out PORTA, r16
-
-	ldi r16, 0x00
-	out TWBR, r16
-	ldi r16, 0x02
-	out TWSR, r16
 main:
-	///////////DUMMY///////
-	call start
-	call sendAddress
-	call stop
-	///////////////////////
-	call start
+	ldi r16, LOW(15752)
+	ldi r17, HIGH(15752)
 
-	call sendAddress
+	ldi r18, LOW(16039)
+	ldi r19, HIGH(16039)
+	call multi16
+	jmp main
 
-	call sendData
+///////////////////////////////////////////////////////////////////////////
+multi16: //MULTI1 i R17:R16 MULTI2 i R19:R18
+	//RESULTAT I R21:R20:R19:R18
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+	push r22
 
-	call repeated_start
-	call readData
-//	call repeated_start
-//	call sendAddress
-doneLoop:
-	call stop
-	rjmp doneLoop
-	/////////////////////
-start:
-	ldi r16, (1<<TWINT) | (1<<TWSTA) | (1<<TWEN)
-	out TWCR, r16 //start
+	clr r22 //ZERO
+	mul r17, r19 //MSB1*MSB2
+	mov r4, r0
+	mov r5, r1
 
-wait1:
-	in r16, TWCR
-	sbrs r16, TWINT
-	rjmp wait1 //v�nta p� ok till start
+	mul r16, r18 //LSB1*LSB2
+	mov r2, r0
+	mov r3, r1
 
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x08
-//	brne ERROR1 //koll om status register �r r�tt annars error
-	ret
-/////////////////////
-stop:
-	ldi r16, (1<<TWINT) | (1<<TWSTO) | (1<<TWEN)
-	out TWCR, r16 //stop
-	ret
-/////////////////////
-repeated_start:
-	ldi r16, (1<<TWINT) | (1<<TWSTA) | (1<<TWEN)
-	out TWCR, r16 //start
-	call TWI_WAIT
-	
+	mul r17, r18 //MSB1*LSB2
+	add r3, r0
+	adc r4, r1
+	adc r5, r22 
 
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x10//0x10 OM REPEATED START!
-	brne ERROR2 //koll om status register �r r�tt annars error
-	ret
-////////////////////////////
+	mul r16, r19 //LSB1*MSB2
+	add r3, r0
+	adc r4, r1
+	adc r5, r22 
 
-sendAddress:
-	ldi r16, 0x29 //SLA is slave adress, W is write bit
-	lsl r16
-	ori r16, 0 //s�tter write bit
-	out TWDR, r16
-	ldi r16, (1<<TWINT) | (1<<TWEN)
-	out TWCR, r16 //skicka SLA+W f�r att aktivera MT mode
-	call TWI_WAIT
-
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x18
-	//brne ERROR3 //koll om status register �r r�tt annars error
-	ret
-///////////////////////
-	
-	// DATA KAN NU L�GGAS TILL TWDR //
-sendData:
-	ldi r16, IDcommand //H�r laddas data
-	ori r16, 0x80
-	out TWDR, r16 //laddad
-
-	ldi r16, (1<<TWINT) | (1<<TWEN)
-	out TWCR, r16 //skjut!
-	call TWI_WAIT
-
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x28
-	brne ERROR4 //koll om status register �r r�tt annars error
-	ret
-/////////////////////////
-
-
-readData:
-	
-
-sendAddressRead:
-	ldi r16, 0x29 //SLA is slave adress, W is write bit
-	lsl r16
-	ori r16, 1 //s�tter read bit
-	out TWDR, r16
-	ldi r16, (1<<TWINT) | (1<<TWEN) //| (1<<TWEA)
-	out TWCR, r16 //skicka SLA+W f�r att aktivera MT mode
-	call TWI_WAIT
-
-
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x40
-	brne ERROR5 //koll om status register �r r�tt annars error
-
-	ldi r16, (1<<TWINT) | (1<<TWEN) | (1<<TWEA)
-	out TWCR, r16
-	call TWI_WAIT
-
-	in r16, TWSR
-	andi r16, 0xF8
-	cpi r16, 0x50
-	brne ERROR6 //koll om status register �r r�tt annars error
-
-	in r16, TWDR
-	out PORTA, r16
-	ret
-TWI_WAIT:
-	in r16, TWCR
-	sbrs r16, TWINT
-	rjmp TWI_WAIT
-	ret
-ERROR1:
-	ldi r16, 0x01
-	out porta, r16
-	call stop
-	jmp error1
-ERROR2:
-	ldi r16, 0x02
-	out porta, r16
-	call stop
-	jmp error2
-ERROR3:
-	ldi r16, 0x04
-	out porta, r16
-	call stop
-	jmp error3
-ERROR4:
-	ldi r16, 0x08
-	out porta, r16
-	call stop
-	jmp error4
-ERROR5:
-	ldi r16, 0x10
-	out porta, r16
-	call stop
-	jmp error5
-ERROR6:
-	ldi r16, 0x10
-	out porta, r16
-	call stop
-	jmp error6
-DELAY_INIT:
-	ldi r16, 0xFF
-	ldi r17, 0xFF
-DELAY_INIT1:
-	dec r16
-	brne DELAY_INIT1
-DELAY_INIT2:
-	dec r17
-	brne DELAY_INIT1
+	mov r18, r2
+	mov r19, r3
+	mov r20, r4
+	mov r21, r5
+	pop r22
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
 	ret
